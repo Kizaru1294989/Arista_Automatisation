661035107e16a8da9e47da336205c7bbc98170fe
Kizaru1294989 <ryan.rais@ynov.com>
2024-04-23 15:31:06 +0200

    zindar

--- yml/main.py
+++ yml/main.py
@@ -11,30 +11,31 @@ def run_ansible_playbook(playbook_path):
         return True
     
 def Leaf_mlag():
+    #################################
     print("MLAG PEER LINK - LEAF-1 <=> LEAF-2 \n")
     print("LEAF_1")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_1/Leaf_1/Leaf1.yml")
     print("LEAF_2")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_1/Leaf_2/Leaf2.yml")
-    
+    #################################
     print("MLAG PEER LINK - LEAF-3 <=> LEAF-4 \n")
     print("LEAF_3")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_2/Leaf_3/Leaf3.yml")
     print("LEAF_4")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_2/Leaf_4/Leaf4.yml")
-    
+    #################################
     print("MLAG PEER LINK - LEAF-5 <=> LEAF-6")
     print("LEAF_5")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_3/Leaf_5/Leaf5.yml")
     print("LEAF_6")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_3/Leaf_6/Leaf6.yml")
-    
+    #################################
     print("MLAG PEER LINK - LEAF-7 <=> LEAF-8")
     print("LEAF_7")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_4/Leaf_7/Leaf7.yml")
     print("LEAF_8")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/MLAG/Peer_Link_Leef/peer_link_4/Leaf_8/Leaf8.yml")
-    
+    #################################
     
 def Spine_mlag():
     print("SPINE_1")
@@ -57,8 +58,37 @@ def mlag():
         
 def bgp():
     print("BGP")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/connectivity/get-version.yml")
     print("NO MLAG SPINE")
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/no_mlag_spine.yml")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/NO_MLAG/no_mlag_spine.yml")
+    ################################
+    # print("SPINE_1")
+    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_1/Spine.yml")
+    # print("SPINE_2")    
+    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_2/Spine.yml")
+    print("SPINE_3") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_3/Spine.yml")
+    print("SPINE_4") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_4/Spine.yml")
+    #################################
+    # print("LEAF_1") 
+    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_1/Leaf1.yml")
+    # print("LEAF_2") 
+    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_2/Leaf2.yml")
+    # print("LEAF_3") 
+    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_3/Leaf3.yml")
+    # print("LEAF_4") 
+    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_4/Leaf4.yml")
+    #################################
+    print("LEAF_5") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_5/Leaf5.yml")
+    print("LEAF_6") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_6/Leaf6.yml")
+    print("LEAF_7") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_7/Leaf7.yml")
+    print("LEAF_8") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_8/Leaf8.yml")
+    #################################
     
         
 def main():
