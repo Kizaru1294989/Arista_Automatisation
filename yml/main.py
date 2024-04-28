67372c2f406493f6c8193a6a44656a8b20d8cadb
Kizaru1294989 <ryan.rais@ynov.com>
2024-04-28 03:30:54 +0200

    re

--- yml/main.py
+++ yml/main.py
@@ -53,47 +53,59 @@ def mlag():
         print("✅")
         Leaf_mlag()
         Spine_mlag()
+        return True
     else:
         print("❌ Error : network unreachable")
+        return False
         
 def bgp():
     print("BGP")
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/connectivity/get-version.yml")
+    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/connectivity/get-version.yml")
     print("NO MLAG SPINE")
     run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/NO_MLAG/no_mlag_spine.yml")
-    ################################
-    # print("SPINE_1")
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_1/Spine.yml")
-    # print("SPINE_2")    
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_2/Spine.yml")
-    print("SPINE_3") 
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_3/Spine.yml")
-    print("SPINE_4") 
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_4/Spine.yml")
+    ###############################
+    print("SPINE_1")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_1/Spine.yml")
+    print("SPINE_2")    
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_2/Spine.yml")
+    #print("SPINE_3") 
+    #run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_3/Spine.yml")
+    #print("SPINE_4") 
+    #run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Spine/Spine_4/Spine.yml")
     #################################
-    # print("LEAF_1") 
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_1/Leaf1.yml")
-    # print("LEAF_2") 
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_2/Leaf2.yml")
-    # print("LEAF_3") 
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_3/Leaf3.yml")
-    # print("LEAF_4") 
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_4/Leaf4.yml")
+    print("LEAF_1") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_1/Leaf1.yml")
+    print("LEAF_2") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_2/Leaf2.yml")
+    print("LEAF_3") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_3/Leaf3.yml")
+    print("LEAF_4") 
+    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_4/Leaf4.yml")
     #################################
-    print("LEAF_5") 
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_5/Leaf5.yml")
-    print("LEAF_6") 
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_6/Leaf6.yml")
-    print("LEAF_7") 
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_7/Leaf7.yml")
-    print("LEAF_8") 
-    run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_8/Leaf8.yml")
+    #print("LEAF_5") 
+    #run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_5/Leaf5.yml")
+    #print("LEAF_6") 
+    #run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_6/Leaf6.yml")
+    #print("LEAF_7") 
+    #run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_7/Leaf7.yml")
+    #print("LEAF_8") 
+    #run_ansible_playbook("/home/rais/Arista_Automatisation/yml/Ansible/BGP/Leaf/Leaf_8/Leaf8.yml")
     #################################
     
         
 def main():
     #mlag()
     bgp()
+    # reponse = mlag()
+    # if reponse :
+    #     choice = input("Transofrm the Lab to BGP lab ? Y/N")
+    #     if choice == "Y":
+    #         bgp()
+    #     elif choice == "N":
+    #         exit(0)
+        
+    # else:
+    #     exit(0)
 
 if __name__ == '__main__':
     main()
