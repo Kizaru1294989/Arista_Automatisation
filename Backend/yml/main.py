b57d8e6db366c22fc501f7b84ad2e8f2396412e2
Kizaru1294989 <ryan.rais@ynov.com>
2024-05-02 17:55:00 +0200

    vxlan

---
+++ Backend/yml/main.py
@@ -0,0 +1,23 @@
+
+from Labs.mlag import *
+from Labs.bgp import *
+from Labs.vxlan import *
+
+def main():
+    print("MAIN")
+    # mlag()
+    # bgp()
+    vxlan()
+    # reponse = mlag()
+    # if reponse :
+    #     choice = input("Transofrm the Lab to BGP lab ? Y/N")
+    #     if choice == "Y":
+    #         bgp()
+    #     elif choice == "N":
+    #         exit(0)
+        
+    # else:
+    #     exit(0)
+
+if __name__ == '__main__':
+    main()
