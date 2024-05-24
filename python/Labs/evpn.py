4cbfca03a2dbaa97fb80fed021963b9411a8bc61
Kizaru1294989 <ryan.rais@ynov.com>
2024-05-24 16:12:17 +0200

    e

--- python/Labs/evpn.py
+++ python/Labs/evpn.py
@@ -50,17 +50,17 @@ def evpn():
     # print("SPINE_4")
     # run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Spine/Spine_4/Spine.yml")
     
-    # print("LEAF_1")
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_1/Leaf1.yml")
-    # ssh_connection("10.43.192.29","65001","1.1.1.1")
+    print("LEAF_1")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_1/Leaf1.yml")
+    ssh_connection("10.43.192.29","65001","1.1.1.1")
     
-    # print("LEAF_2")
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_2/Leaf2.yml")
-    # ssh_connection("10.43.192.30","65001","2.2.2.2")
+    print("LEAF_2")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_2/Leaf2.yml")
+    ssh_connection("10.43.192.30","65001","2.2.2.2")
     
-    # print("LEAF_3")
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_3/Leaf3.yml")
-    # ssh_connection("10.43.192.31","65002","3.3.3.3")
+    print("LEAF_3")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_3/Leaf3.yml")
+    ssh_connection("10.43.192.31","65002","3.3.3.3")
     
     print("LEAF_4")
     run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_4/Leaf4.yml")
@@ -72,17 +72,17 @@ def evpn():
     run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_5/Leaf5.yml")
     ssh_connection("10.43.192.33","65004","5.5.5.5")
     
-    # print("LEAF_6")
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_6/Leaf6.yml")
-    # ssh_connection("10.43.192.34","65004","6.6.6.6")
+    print("LEAF_6")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_6/Leaf6.yml")
+    ssh_connection("10.43.192.34","65004","6.6.6.6")
     
-    # print("LEAF_7")
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_7/Leaf7.yml")
-    # ssh_connection("10.43.192.35","65005","7.7.7.7")
+    print("LEAF_7")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_7/Leaf7.yml")
+    ssh_connection("10.43.192.35","65005","7.7.7.7")
     
-    # print("LEAF_8")
-    # run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_8/Leaf8.yml")
-    # ssh_connection("10.43.192.36","65005","8.8.8.8")
+    print("LEAF_8")
+    run_ansible_playbook("/home/rais/Arista_Automatisation/python/Ansible/EVPN/Leaf/Leaf_8/Leaf8.yml")
+    ssh_connection("10.43.192.36","65005","8.8.8.8")
 
 
     
