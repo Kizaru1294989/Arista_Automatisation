5494e229b0a4463ca1676767cf36d81c40f60b6d
ryan <ryan.rais@ynov.com>
2024-05-31 21:26:59 +0200

    e

--- src/components/pages/Home/HomeComponent.jsx
+++ src/components/pages/Home/HomeComponent.jsx
@@ -1,40 +1,54 @@
-import * as React  from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
-import { CardContent } from '@mui/material';
-import MLAG from '../../../assets/img/MLAG_schema.png';
+import { CardContent, Button } from '@mui/material';
+import MLAG from '../../../assets/img/mlag.png';
+import BGP from '../../../assets/img/bgp.png';
 import HomeCard from './HomeCard';
 
-export default function MediaControlCard({lab,status , setLoadingDialog,formValue}) {
-  // console.log(labStatus)
-  // let parsedLabStatus = JSON.parse(labStatus.labStatus.replace(/'/g, '"'));
-  // let lab = Object.keys(parsedLabStatus)[0];
-  // let status = parsedLabStatus[lab];
-  
-  console.log(lab); 
-  console.log(status);
+const labs = [
+  { name: 'MLAG', image: MLAG, title: 'Lab Arista étape I : MLAG' },
+  { name: 'BGP', image: BGP, title: 'Lab Arista étape II : BGP' },
+];
+
+export default function MediaControlCard({ lab, status, setLoadingDialog, formValue }) {
+  const [currentLabIndex, setCurrentLabIndex] = useState(0);
+
+  const handleNextLab = () => {
+    setCurrentLabIndex((prevIndex) => (prevIndex + 1) % labs.length);
+  };
+
+  const handlePrevLab = () => {
+    setCurrentLabIndex((prevIndex) => (prevIndex - 1 + labs.length) % labs.length);
+  };
+
   return (
     <>
-    <Grid sx={{ display: 'flex'  , backgroundColor : '#2d3436', backgroundImage : 'linear-gradient(315deg, #2d3436 0%, #000000 74%)' , width : '300px'}}>
-      <Grid sx={{  display: 'flex', flexDirection: 'column' }}>
-        <HomeCard setLoadingDialog={setLoadingDialog} formValue={formValue}/>
-        <Grid sx={{ display: 'flex', alignItems: 'center' ,justifyContent : 'center' , margin : '125px' , pl: 1, pb: 1 }}>  
+      <Grid sx={{ display: 'flex', backgroundColor: '#2d3436', backgroundImage: 'linear-gradient(315deg, #2d3436 0%, #000000 74%)', width: '300px' }}>
+        <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+          <HomeCard setLoadingDialog={setLoadingDialog} formValue={formValue} />
+          <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '125px', pl: 1, pb: 1 }}>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
-    
-    <Grid  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'  }}>
 
-        <Card >
-          
-          <CardContent sx={{ backgroundColor : 'black'}}>
-          <Grid>
-              <h1 style={{ color : 'white'}}>Lab Arista étape I : MLAG</h1>
-             
+      <Grid sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+        <Card>
+          <CardContent sx={{ backgroundColor: 'black' }}>
+            <Grid>
+              <h1 style={{ color: 'white' }}>{labs[currentLabIndex].title}</h1>
             </Grid>
             <Grid>
-              <h1 style={{ color : 'white'}}>Etape actuelle : {lab + " LAB " + status}</h1>
-              <img src={MLAG} alt="Votre Image" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+              <h1 style={{ color: 'white' }}>Etape actuelle : {lab + " LAB " + status}</h1>
+              <img src={labs[currentLabIndex].image} alt="Lab Image" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+            </Grid>
+            <Grid sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+              <Button variant="contained" color="primary" onClick={handlePrevLab}>
+                Lab Précédent
+              </Button>
+              <Button variant="contained" color="primary" onClick={handleNextLab}>
+                Lab Suivant
+              </Button>
             </Grid>
           </CardContent>
         </Card>
